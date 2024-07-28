e59fef2b852d937d5f82bbccd0eedb8cc71a3071
AkaCMD <akacmd@foxmail.com>
2024-07-28 20:56:09 +0800

    Item floating effect

--- entry_cmdgame.c
+++ entry_cmdgame.c
@@ -4,18 +4,9 @@ const float entity_selection_radius = 18.0f;
 const int rock_health = 3;
 const int tree_health = 3;
 
-int world_pos_to_tile_pos(float world_pos) {
-	return world_pos / (float)tile_width;
-}
-
-float tile_pos_to_world_pos(int tile_pos) {
-	return (float)tile_pos * (float)tile_width;
-}
-
-Vector2 round_v2_to_tile(Vector2 world_pos) {
-	world_pos.x = tile_pos_to_world_pos(world_pos_to_tile_pos(world_pos.x));
-	world_pos.y = tile_pos_to_world_pos(world_pos_to_tile_pos(world_pos.y));
-	return world_pos;
+// 0 -> 1
+float sin_breath(float time, float rate) {
+	return (sin(time * rate) + 1.0) / 2.0;
 }
 
 bool almost_equals(float a, float b, float epsilon) {
@@ -34,6 +25,22 @@ bool animate_f32_to_target(float* value, float target, float delta_t, float rate
 void animate_v2_to_target(Vector2* value, Vector2 target, float delta_t, float rate) {
 	animate_f32_to_target(&(value->x), target.x, delta_t, rate);
 	animate_f32_to_target(&(value->y), target.y, delta_t, rate);
+}
+// :utils
+
+
+int world_pos_to_tile_pos(float world_pos) {
+	return world_pos / (float)tile_width;
+}
+
+float tile_pos_to_world_pos(int tile_pos) {
+	return (float)tile_pos * (float)tile_width;
+}
+
+Vector2 round_v2_to_tile(Vector2 world_pos) {
+	world_pos.x = tile_pos_to_world_pos(world_pos_to_tile_pos(world_pos.x));
+	world_pos.y = tile_pos_to_world_pos(world_pos_to_tile_pos(world_pos.y));
+	return world_pos;
 }
 
 typedef enum EntityArchetype {
@@ -81,6 +88,7 @@ typedef struct Entity {
 	SpriteID sprite_id;
 	int health;
 	bool destroyable_world_item;
+	bool is_item;
 } Entity;
 // :entity
 #define MAX_ENTITY_COUNT 1024
@@ -135,11 +143,13 @@ void setup_tree(Entity* en) {
 void setup_item_rock(Entity* en) {
 	en->arch = arch_item_rock;
 	en->sprite_id = SPRITE_item_rock;
+	en->is_item = true;
 }
 
 void setup_item_pine_wood(Entity* en) {
 	en->arch = arch_item_pine_wood;
 	en->sprite_id = SPRITE_item_pine_wood;
+	en->is_item = true;
 }
 
 Vector2 screen_to_world() {
@@ -330,6 +340,11 @@ int entry(int argc, char **argv) {
 					{
 						Sprite* sprite = get_sprite(en->sprite_id);
 						Matrix4 x_form = m4_scalar(1.0);
+
+						if (en->is_item) {
+							x_form = m4_translate(x_form, v3(0, 2.0 * sin_breath(os_get_current_time_in_seconds(), 5.0), 0));
+						}
+
 						x_form = m4_translate(x_form, v3(0, tile_width * -0.5, 0));
 						x_form = m4_translate(x_form, v3(en->pos.x, en->pos.y, 0));
 						x_form = m4_translate(x_form, v3(sprite->image->width * -0.5, 0.0, 0.0));
